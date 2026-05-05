332fc3d82005b07c16584bfe99532eca05fe4cc1
Angel-OF-Immorality <12chiragconquerer@gmail.com>
2026-05-05 18:27:39 +0000

    Fixed one Black Formatting issue

--- app/database.py
+++ app/database.py
@@ -14,7 +14,7 @@ DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./shortener.db")
 engine = create_engine(
     DATABASE_URL,
     connect_args=(
-    {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+        {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
     ),
 )
 
